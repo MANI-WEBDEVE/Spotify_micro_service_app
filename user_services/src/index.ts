4df45886e10bb8095b034bd2233eb89b32ef44f6
MANI-WEBDEVE <nudmaufon@gmail.com>
2025-07-09 23:18:12 +0500

    Update user services configuration and implement MongoDB connection
    - Changed package.json to set module type to 'module' and added build and start scripts.
    - Updated tsconfig.json to refine compiler options and ensure consistent casing in file names.
    - Enhanced index.ts to include dotenv for environment variable management and established a MongoDB connection using mongoose.

--- user_services/src/index.ts
+++ user_services/src/index.ts
@@ -1,8 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-
+dotenv.config();  
+ 
+const connectDB = async () => {
+  console.log(process.env.MONGODB_URI); 
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log("Connected to MongoDB"); 
+  } catch (error) {
+    console.log(error);
+    process.exit(1);   
+  }
+} 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
